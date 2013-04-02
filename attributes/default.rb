b2d412b1ea920295c54a0f93552b3c6c86f9c83c
Jeff Jagoda <jsjagoda@us.ibm.com>
2013-04-02 14:09:17 -0400

    Revert "HTTPS URLs seem to break -- switching to HTTP."
    This reverts commit d4623d7a3da50af3d234b3ee459a047aa78d3ecd.

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,13 +14,13 @@ default['graphite']['storage_dir'] = "/opt/graphite/storage"
 default['graphite']['timezone'] = "America/Los_Angeles"
 default['graphite']['django_root'] = "@DJANGO_ROOT@"
 
-default['graphite']['whisper']['uri'] = "http://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/whisper-#{node['graphite']['version']}.tar.gz"
+default['graphite']['whisper']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/whisper-#{node['graphite']['version']}.tar.gz"
 default['graphite']['whisper']['checksum'] = "36b5fa917526224678da0a530a6f276d00074f0aa98acd6e2412c79521f9c4ff"
 
-default['graphite']['graphite_web']['uri'] = "http://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/graphite-web-#{node['graphite']['version']}.tar.gz"
+default['graphite']['graphite_web']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/graphite-web-#{node['graphite']['version']}.tar.gz"
 default['graphite']['graphite_web']['checksum'] = "4fd1d16cac3980fddc09dbf0a72243c7ae32444903258e1b65e28428a48948be"
 
-default['graphite']['carbon']['uri'] = "http://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/carbon-#{node['graphite']['version']}.tar.gz"
+default['graphite']['carbon']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/carbon-#{node['graphite']['version']}.tar.gz"
 default['graphite']['carbon']['checksum'] = "4f37e00595b5b078edb9b3f5cae318f752f4446a82623ea4da97dd7d0f6a5072"
 default['graphite']['carbon']['line_receiver_interface'] =   "0.0.0.0"
 default['graphite']['carbon']['line_receiver_port'] = 2003
